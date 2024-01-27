23740937f229a797db71fc3f8591379089bb643e
DayMar-SCRIPTER <134222932+DayMar-SCRIPTER@users.noreply.github.com>
2024-01-28 00:46:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Sleduje  3 SERVERY","CREATED BY  DAYMAR_DEV","Kontroluje SERVERS"];
 
 
 let currentIndex = 0;
